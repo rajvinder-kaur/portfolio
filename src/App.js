aad2b7030457b5ce39c583ac9ed28997aae856ec
Sujal Garg <100895523+sujal-garg@users.noreply.github.com>
2023-02-25 17:48:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { React, Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Switch } from "react-router";
 import "./App.css";
